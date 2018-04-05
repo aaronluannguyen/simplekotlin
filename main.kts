632903da87fa75f6fd155d679fcd6e484cee34e7
aaronluannguyen <aaronluannguyen@gmail.com>
2018-04-04 20:55:01 -0700

    Passed sub tests

--- main.kts
+++ main.kts
@@ -20,6 +20,8 @@ fun whenFn(arg: Any): String {
 val add = {int1: Int, int2: Int -> (int1 + int2)}
 
 // write a "sub" function that takes two Ints, returns an Int, and subtracts the values
+val sub = {int1: Int, int2: Int -> (int1 - int2)}
+
 // write a "mathOp" function that takes two Ints and a function (that takes two Ints and returns an Int), returns an Int, and applies the passed-in-function to the arguments
 
 // write a class "Person" with first name, last name and age
@@ -56,18 +58,18 @@ for ( (k,v) in add_tests) {
 }
 println("")
 
-//print("Sub tests: ")
-//val sub_tests = listOf(
-//    Pair(0, 0) to 0,
-//    Pair(2, 1) to 1,
-//    Pair(-2, 2) to -4,
-//    Pair(456, 123) to 333
-//)
-//for ( (k,v) in sub_tests) {
-//    print(if (sub(k.first, k.second) == v) "." else "!")
-//}
-//println("")
-//
+print("Sub tests: ")
+val sub_tests = listOf(
+    Pair(0, 0) to 0,
+    Pair(2, 1) to 1,
+    Pair(-2, 2) to -4,
+    Pair(456, 123) to 333
+)
+for ( (k,v) in sub_tests) {
+    print(if (sub(k.first, k.second) == v) "." else "!")
+}
+println("")
+
 //print("Op tests: ")
 //print(if (mathOp(2, 2, { l,r -> l+r} ) == 4) "." else "!")
 //print(if (mathOp(2, 2, ::add ) == 4) "." else "!")
